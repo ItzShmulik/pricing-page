d8d54354ddf577ddd47f2a84eed9aa9f7f17c384
shmulik55 <gshloman@gmail.com>
2025-03-03 22:34:00 +0200

    Finished Plans CSS

--- src/Components/DataBox.jsx
+++ src/Components/DataBox.jsx
@@ -3,7 +3,10 @@ function DataBox(props) {
     <div className="plan-box">
       <h2>{props.title}</h2>
       <p className="plan-description">{props.description}</p>
-      <h2>${props.price}/MONTH</h2>
+      <h2 className="plan-pricing-details">
+        <span className="plan-currency">$</span>
+        <span className="plan-price">{props.price}</span>/ MONTH
+      </h2>
       {props.perks}
       <button className="start-free-button">START FREE</button>
     </div>
